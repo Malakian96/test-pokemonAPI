c6e2cf3048b116c5ccb99dfa510656122f2138eb
Jude <Jude@MacBook-Air-de-Jude.local>
2021-07-01 01:46:48 +0200

    Fixed lints

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
+    'react/jsx-filename-extension': 0,
+    'import/no-unresolved': 0
   },
 };
--- src/application/App.js
+++ src/application/App.js
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
-import http from "@Common/http";
-import RouteWithProps from "@Application/components/RouteWithProps/RouteWithProps";
+import {
+  BrowserRouter as Router, Switch,
+} from 'react-router-dom';
+import http from '@Common/http';
+import RouteWithProps from '@Application/components/RouteWithProps/RouteWithProps';
 import './App.css';
 import Pokedex from '@Pages/Pokedex/Pokedex';
 import Detail from '@Common/components/Detail/Detail';
 
 const POKEMON_LIMIT = 151;
 const App = () => {
-    const [pokemons, setPokemons] = useState([]);
-    const [pokemon, setPokemon] = useState({});
-    
-    useEffect(async () => {
-        const arrPokemons = await http.get(`/pokemon?limit=${POKEMON_LIMIT}`)
-        setPokemons(arrPokemons.data.results);
-    },[]);
+  const [pokemons, setPokemons] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [pokemon, setPokemon] = useState({});
 
-    return(
+  useEffect(async () => {
+    const arrPokemons = await http.get(`/pokemon?limit=${POKEMON_LIMIT}`);
+    setPokemons(arrPokemons.data.results);
+  }, []);
+
+  return (
     <Router>
       <Switch>
-        <RouteWithProps path="/" exact component={Pokedex} data={{pokemons:pokemons, setPokemon:setPokemon}} />
-        <RouteWithProps path="/:name" component={Detail}/>
+        <RouteWithProps path="/" exact component={Pokedex} data={{ pokemons, setPokemon }} />
+        <RouteWithProps path="/:name" component={Detail} />
       </Switch>
     </Router>
-    );
+  );
 };
 
 export default App;
